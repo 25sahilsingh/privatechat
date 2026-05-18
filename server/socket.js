d9dd791c310034bc3fbfdf656e0c638e2bdb70eb
Sahil Singh <25.sahilsingh@gmail.com>
2026-05-18 14:10:25 +0530

    changing port to env

--- server/socket.js
+++ server/socket.js
@@ -4,8 +4,10 @@ import cors from "cors";
 const app = express();
 app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
 app.use(express.static("public"));
-const expressServer = app.listen(5000, "0.0.0.0", () => {
-  console.log("server is up and running");
+const PORT = process.env.PORT || 5000; // Render injects PORT automatically
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 const io = new Server(expressServer, {
   cors: {
